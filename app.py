fa72a791e8c1caf43b7dc58702fc63363699cfe1
Cristopher Yates <cmoyates@gmail.com>
2021-10-21 20:52:37 -0230

    Major overhaul

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, send_file, request
+import io
+from fastapi import FastAPI, responses
 from PIL import Image, ImageEnhance, ImageDraw, ImageFont
 from tempfile import NamedTemporaryFile
 from shutil import copyfileobj
@@ -7,86 +8,84 @@ from io import BytesIO
 import requests
 
 
-
-
 #############################################################################
-white = (255,255,255)
-black = (0,0,0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
 
-imageString = None
-contrastScale = 2.5
-saturationScale = 2.5
-sharpenScale = 5
-brightnessScale = 0.65
-font = None
-tx = ty = bx = by = 0
-width = None
-height = None
+CONTRAST_SCALE = 2.5
+SATURATION_SCALE = 2.5
+SHARPEN_SCALE = 5
+BRIGHTNESS_SCALE = 0.65
 
-def ImportImage(imgString):
-    r = requests.get(imgString)
+
+def ImportImage(img_string: str):
+    r = requests.get(img_string)
     img = Image.open(BytesIO(r.content))
     width, height = img.size
-    maxDim = max(width, height)
-    if maxDim > 1024:
-        scale = maxDim / 1024
+    max_dimension = max(width, height)
+    if max_dimension > 1024:
+        scale = max_dimension / 1024
         width = round(width / scale)
         height = round(height / scale)
         img = img.resize((width, height))
-    fontSize = min(width, height)/9
-    font = ImageFont.truetype("impact.ttf", round(fontSize))
+    font_size = min(width, height) / 9
+    font = ImageFont.truetype("impact.ttf", round(font_size))
     return img, width, height, font
 
-def DeepFry(img):
-    img = ImageEnhance.Contrast(img).enhance(contrastScale)
-    img = ImageEnhance.Color(img).enhance(saturationScale)
-    img = ImageEnhance.Sharpness(img).enhance(sharpenScale)
-    img = ImageEnhance.Brightness(img).enhance(brightnessScale)
+
+def DeepFry(img: Image):
+    img = ImageEnhance.Contrast(img).enhance(CONTRAST_SCALE)
+    img = ImageEnhance.Color(img).enhance(SATURATION_SCALE)
+    img = ImageEnhance.Sharpness(img).enhance(SHARPEN_SCALE)
+    img = ImageEnhance.Brightness(img).enhance(BRIGHTNESS_SCALE)
     return img
 
-def AddText(img, topText, bottomText):
+
+def AddText(img: Image, top_text: str, bottom_text: str, font: ImageFont, border_thickness: int, width: int, height: int):
     draw = ImageDraw.Draw(img)
-    tw, th = draw.textsize(topText,font)
-    bw, bh = draw.textsize(bottomText,font)
-    tx = ((width-tw)/2)
-    bx = ((width-bw)/2)
-    ty = (height/15)
-    by = height - (height/15) - bh
+    tw, _th = draw.textsize(top_text, font)
+    bw, bh = draw.textsize(bottom_text, font)
+    tx = (width - tw) / 2
+    bx = (width - bw) / 2
+    ty = height / 15
+    by = height - (height / 15) - bh
     for i in range(4):
-        xOffset = -1 if (i%2) else 1
-        yOffset = -1 if (i < 3) else 1
-        draw.text((tx+xOffset, ty+yOffset), topText, font=font, fill=black)
-        draw.text((bx+xOffset, by+yOffset), bottomText, font=font, fill=black)
-    draw.text((tx, ty), topText, white, font=font)
-    draw.text((bx, by), bottomText, white, font=font)
+        x_offset = -border_thickness if (i % 2) else border_thickness
+        y_offset = -border_thickness if (i < 3) else border_thickness
+        draw.text((tx + x_offset, ty + y_offset), top_text, font=font, fill=BLACK)
+        draw.text((bx + x_offset, by + y_offset), bottom_text, font=font, fill=BLACK)
+    draw.text((tx, ty), top_text, WHITE, font=font)
+    draw.text((bx, by), bottom_text, WHITE, font=font)
     return img
+
+
 #############################################################################
 
 
+app = FastAPI()
 
-app = Flask(__name__)
+@app.get("/api")
+def return_image(top: str, bottom: str, url: str, deep_fry: int):
+    img, width, height, font = ImportImage(url)
+    file_name = "car.jpg"
 
-@app.route("/api", methods=["GET"])
-def hello_world():
+    new_img = AddText(
+        img, top, bottom, font, 2, width, height
+    )
+    if deep_fry == 1:
+        new_img = DeepFry(new_img)
 
-    Query = str(request.args["query"])
-    data = Query.split("|")
-    print(data)
-    global width, height, font
-    img, width, height, font = ImportImage(data[0])
-    c = "car.jpg"
-    newImg = DeepFry(AddText(img, data[1], data[2])) if (data[3]=="1") else AddText(img, data[1], data[2])
-    newImg = newImg.convert("RGB")
-    newImg.save(c)
-    tempFileObj = NamedTemporaryFile(mode='w+b',suffix='jpg')
-    pilImage = open(c,'rb')
-    copyfileobj(pilImage,tempFileObj)
-    pilImage.close()
-    remove(c)
-    tempFileObj.seek(0,0)
-    
-    response = send_file(tempFileObj, as_attachment=True, attachment_filename=c)
-    return response
+    new_img = new_img.convert("RGB")
+    new_img.save(file_name)
+    temp_file_obj = NamedTemporaryFile(mode="w+b", suffix="jpg")
+    pil_image = open(file_name, "rb")
+    copyfileobj(pil_image, temp_file_obj)
+    pil_image.close()
+    remove(file_name)
+    temp_file_obj.seek(0, 0)
+
+    return responses.StreamingResponse(io.BytesIO(temp_file_obj.read()), media_type="image/jpg")
+
 
 if __name__ == "__main__":
     app.run()
